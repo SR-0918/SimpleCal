6b59b2e4307c543a4100b4ca53150a19aa1929a3
Yuka-Yukka <95258095+Yuka-Yukka@users.noreply.github.com>
2021-12-10 12:43:21 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="toppage.css">
+    <title>toppage</title>
+</head>
+<body>
+<header>
+    <div class="header-set">
+        <div class="header-right">
+            <div class="tab-right">
+                <a href="">TOP &ensp;</a>
+            </div>
+            <div class="tab-right">
+                <a href="filter.php">デジタルフィルタ設計 &ensp;</a>
+            </div>
+            <div class="tab-right">
+                <a href="radius.php">面積  &ensp;</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="about">
+    <?='ここでは簡単な計算が行えます。'?><br />
+    <?='上のタブから計算したいものを選択して行ってください'?><br />
+
+
+</div>
+    
+</body>
+</html>
